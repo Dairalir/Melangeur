dcfbd80d9d497700a82669d7aece81f6e87793e3
Dairalir <daimanvm@gmail.com>
2023-01-11 16:29:17 +0100

    Melangeur final

--- src/main/java/com/example/melangeur_couleur/App.java
+++ src/main/java/com/example/melangeur_couleur/App.java
@@ -12,7 +12,7 @@ import java.util.Objects;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("checkbox.fxml")));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Melangeur.fxml")));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/melangeur_couleur/GUI/MelangeurController.java
+++ src/main/java/com/example/melangeur_couleur/GUI/MelangeurController.java
@@ -2,6 +2,7 @@ package com.example.melangeur_couleur.GUI;
 
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
 
 public class MelangeurController {
 
@@ -9,4 +10,25 @@ public class MelangeurController {
     public Slider red_slider;
     public Slider green_slider;
     public Slider blue_slider;
+
+    public void Slide_Red(MouseEvent mouseEvent) {
+        double sred = red_slider.getValue();
+        double sgreen = green_slider.getValue();
+        double sblue = blue_slider.getValue();
+        color_result.setStyle("-fx-background-color: rgb(" + sred + "," + sgreen + "," + sblue + ")");
+    }
+
+    public void Slide_Green(MouseEvent mouseEvent) {
+        double sred = red_slider.getValue();
+        double sgreen = green_slider.getValue();
+        double sblue = blue_slider.getValue();
+        color_result.setStyle("-fx-background-color: rgb(" + sred + "," + sgreen + "," + sblue + ")");
+    }
+
+    public void Slide_Blue(MouseEvent mouseEvent) {
+        double sred = red_slider.getValue();
+        double sgreen = green_slider.getValue();
+        double sblue = blue_slider.getValue();
+        color_result.setStyle("-fx-background-color: rgb(" + sred + "," + sgreen + "," + sblue + ")");
+    }
 }
